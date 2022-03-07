4555cf725d51d39a3cdb0ec6993d6e2b82c95254
YeinYoon <yeinyun23@gmail.com>
2022-03-07 09:53:48 +0900

    merge commit

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div>
 
     <div id="bar">
-    <router-link to="/" id="main">메인으로 ㄱㄱ</router-link>
-    <h5 class="text-center">대충 뭐시기 메인바</h5>
+    <router-link to="/" id="main">메인으로</router-link>
+    <h5 class="text-center">테스트 메인바</h5>
     <div v-if="isLogin == true" class="userName">{{userName}}</div>
-    <div v-else class="userName">로그인하면 니 이름 뜨는 자리</div>
+    <div v-else class="userName">로그인이 필요합니다</div>
     </div>
 
     <router-view></router-view>
@@ -20,8 +20,10 @@ export default {
   created() {
     axios.post('/api/user/test', {id:123, pw:123})
     .then((result)=>{
-      console.log("DB접근 및 접속 성공");
-      console.log(result);
+      console.log(result.data.rows);
+      result.data.rows.forEach((r, i)=>{
+        console.log(`${i+1}행 : ${r.ID}, ${r.PW}`);
+      })
     })
     .catch((err)=>{
       console.log(err);
