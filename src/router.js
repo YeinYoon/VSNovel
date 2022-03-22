36631622feefa9bb3be7e43fd3cd502756538c57
GitSeokbeom <93910030+GitSeokbeom@users.noreply.github.com>
2022-03-22 19:45:01 +0900

    프론트 개발환경 초기화

--- src/router.js
+++ src/router.js
@@ -1,45 +1,45 @@
-import { createWebHistory, createRouter } from "vue-router";
+// import { createWebHistory, createRouter } from "vue-router";
 
-// 메인페이지
-import Index from './components/Index.vue';
-// 유저(auth)
-import Signup from './components/auth/Signup.vue';
-import Login from './components/auth/Login.vue';
-// 게시판(board)
-import MainBoard from './components/board/MainBoard.vue';
-import Posting from './components/board/Posting.vue';
-import PostInfo from './components/board/PostInfo.vue';
+// // 메인페이지
+// import Index from './components/Index.vue';
+// // 유저(auth)
+// import Signup from './components/auth/Signup.vue';
+// import Login from './components/auth/Login.vue';
+// // 게시판(board)
+// import MainBoard from './components/board/MainBoard.vue';
+// import Posting from './components/board/Posting.vue';
+// import PostInfo from './components/board/PostInfo.vue';
 
-const routes = [
-  {
-    path:"/",
-    component : Index
-  },
-  {
-    path: "/signup",
-    component: Signup,
-  },
-  {
-    path : "/signin",
-    component : Login
-  },
-  {
-    path : "/board",
-    component : MainBoard,
-  },
-  {
-    path: "/posting",
-    component : Posting
-  },
-  {
-   path:"/post/:id",
-   component : PostInfo
-  }
-];
+// const routes = [
+//   {
+//     path:"/",
+//     component : Index
+//   },
+//   {
+//     path: "/signup",
+//     component: Signup,
+//   },
+//   {
+//     path : "/signin",
+//     component : Login
+//   },
+//   {
+//     path : "/board",
+//     component : MainBoard,
+//   },
+//   {
+//     path: "/posting",
+//     component : Posting
+//   },
+//   {
+//    path:"/post/:id",
+//    component : PostInfo
+//   }
+// ];
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes,
+// });
 
-export default router; 
+// export default router; 
