534713579ec4fb2460e920ae6b0dc0ac04088e60
guwonmo <31229143+guwonmo@users.noreply.github.com>
2022-03-30 15:24:30 +0900

    수정

--- src/components/content.vue
+++ src/components/content.vue
@@ -1,52 +1,39 @@
 <template>
-<div>
-  <div v-bind:class="{'contentbackground':true}">
-    <excomp v-bind:class="{'excomp':true}">
-    </excomp>
-  </div>
-</div>
+  <div>
+    <div v-bind:class="{ contentbackground: true }">
+      <excomp v-bind:class="{ excomp: true }"> </excomp>
 
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 <style>
-
 .contentbackground {
   position: absolute;
-  background:#353535;
-  width: calc(100vw - 425px);
+  background: #353535;
+  width: 100%;
   height: 100vh;
-  overflow:auto;
-  
+  z-index: 2;
+  overflow: auto;
 }
 .excomp {
-  /* background:white; */
   position: absolute;
   top: 60px;
-  left: 50px;
-  width: calc(100% - 100px);
-  height: calc(100vh - 60px);
-  overflow:auto;
+  /* left: 10%; */
+  width: 100%;
+  /* height: calc(100vh - 60px); */
+  z-index: 1;
+  overflow: auto;
 }
 </style>
 <script>
-// import excomp from './lib_gallery';
-// import excomp from './mypage_post';
-// import excomp from './mypage_review';
-// import excomp from './lib_list.vue';
-// import excomp from './mypage_review.vue'
-// import excomp from './mypage_prefer.vue'
-// import excomp from './mypage_profile';
-// import excomp from './mypage_card';
-import excomp from './mypage_alarm';
-// import excomp from './mypage_exit.vue';
-// import excomp from './mypage_exit-first.vue';
-
+import excomp from "./MainPage/Main";
 export default {
-  name: 'vsncontent',
+  name: "vsncontent",
 
   components: {
     excomp,
   },
-}
-  
+};
 </script>
 
