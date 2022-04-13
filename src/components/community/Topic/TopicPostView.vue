053e72bf0027511dcaa00ed43e2887ce4847c700
dptmf1 <60639176+dptmftkdghk1@users.noreply.github.com>
2022-04-13 14:15:22 +0900

    comment 디자인 수정

--- src/components/community/Topic/TopicPostView.vue
+++ src/components/community/Topic/TopicPostView.vue
@@ -3,10 +3,10 @@
     <div class="postview_section">
       <div class="postview_title"><div class="postview"><span>{{topicObject.title}}</span></div></div>
       <div class="postview_frame">
-        <div class="postview_content" id="addpostdata"><span v-html="topicObject.content"></span></div>
+        <div class="postview_content"><span v-html="topicObject.content"></span></div>
         <div class="content_vote">
-          <div><span class="vote_btn_ok">추천</span></div>
-          <div><span class="vote_btn_no">비추천</span></div>
+          <div class="vote_btn_ok"><span>추천</span></div>
+          <div class="vote_btn_no"><span>비추천</span></div>
         </div>
       </div>
     </div>
@@ -56,7 +56,7 @@ export default {
 .postview_title span {
   display: table-cell; 
   vertical-align:middle; 
-  /* height: 50px;  px로 주면 안된다?*/
+  height: 50px; 
 }
 .postview {
   position: relative;
@@ -68,11 +68,10 @@ export default {
 }
 .postview_frame {
   color:white;
-  margin: 0 auto;
+  margin: 5px auto;
   width: 100%;
   background-color: #5e5e5e;
   border-radius: 20px;
-  padding: 0 10px;
 }
 .postview_content {
   width: 100%;
@@ -82,41 +81,67 @@ export default {
   padding: 10px 10px;
   overflow-y:scroll;
 }
+.postview_content span {
+  position: relative;
+  left: 5px;
+}
 .content_vote {
     display:flex;
     justify-content: center;
+    position:relative;
     bottom: 20px;
-    margin-top: 50px;
-    position: relative;
+    margin-top: 10px;
     width: 100%;
     height: 50px;
 }
 .vote_btn_ok {
-  padding: 20px 35px;
+  display: table;
+  width : 100px;
+  height: 100%;
   margin: 0 10px;
   background-color: #2872f9;
   border-radius: 20px;
   color:white;
   font-size: 1.5em;
+  cursor:pointer;
+}
+.vote_btn_ok span{
+  display: table-cell; 
+  vertical-align:middle; 
+  text-align: center;
 }
 .vote_btn_no {
-  padding: 20px 30px;
+  display: table;
+  width : 100px;
+  height: 100%;
   margin: 0 10px;
   background-color: red;
   border-radius: 20px;
   color:white;
   font-size: 1.5em;
+  cursor:pointer;
+}
+.vote_btn_no span{
+  display: table-cell; 
+  vertical-align:middle; 
+  text-align: center;
 }
 .postview_btn_area {
+  /* margin: 0 auto; */
+  /* margin: 10px; */
+  display: flex;
+  /* background-color:white; */
+  /* height: 10px; */
   position: relative;
-  top: 70px;
+  top: 50px;
   width: 100%;
   text-align: center;
   color: white;
 }
 .postview_cancle_btn {
   padding: 5px 10px;
-  margin: 0 10px;
+  margin-top: 1%;
+  margin-bottom: 1%;
   background-color: #2872f9;
   width: 100px;
   border-radius: 14px;
