ffd35c865d33b63f84384be9269548ba48f506a6
guwonmo <31229143+guwonmo@users.noreply.github.com>
2022-06-10 09:59:38 +0900

    6/10 카페수정

--- src/components/MainPage/Main.vue
+++ src/components/MainPage/Main.vue
@@ -393,7 +393,7 @@ button {
   background-color: #494949;
   padding: 5px 10px 0 10px;
   border-radius: 20px;
-  overflow-y: scroll;
+  /* overflow-y: scroll; */
 }
 .store ::-webkit-scrollbar {
   display: block;
--- src/components/side/StoreSide.vue
+++ src/components/side/StoreSide.vue
@@ -120,10 +120,6 @@ export default {
 <style>
 /* -------------------------------------------------------------------- */
 
-::-webkit-scrollbar {
-  display: none;
-}
-
 .side_search {
   background-color: white;
   width: 80%;
